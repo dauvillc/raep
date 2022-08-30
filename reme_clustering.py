77621fc813097b0cbaa656df00e4c1d4ea69905f
Clément Dauvilliers <dauvilliersclem@gmail.com>
2022-08-30 16:12:43 +0200

    Python script for parsing the full doc

--- reme_clustering.py
+++ reme_clustering.py
@@ -21,7 +21,12 @@ from sklearn.cluster import AgglomerativeClustering
 from scipy.spatial import distance_matrix
 
 # Path to the full HTML version of the REME pdf.
-HTML_FILE = os.path.join("docs", "rep_metiers.html")
+HTML_FILE = os.path.join("docs", "rep_metiers-12.html")
+# Path to the new HTML file over which the clusters have been drawed
+NEW_HTML_FILE = os.path.join("visu_docs", "new_rep_metiers-12.html")
+# Whether to draw the clusters on a new HTML file
+DRAW_CLUSTERS = True
+
 # Minimal distance in pixels between two clusters for them
 # to be split during the clustering algorithm.
 CLUSTERING_DISTANCE_THRESHOLD = 37.5
@@ -36,12 +41,14 @@ def get_pos(p_object):
     return int(top), int(left)
 
 
-def page_paragraphs(p_objects):
+def page_paragraphs(p_objects, draw_clusters=False):
     """
     Detects the paragraphs within a Soup corresponding
     to a single document page.
     :param p_objects: <p> elements of the soup within the page
         that is being processed.
+    :param draw_clusters: Boolean, whether to add a background color
+        to the paragraphs to indicate the clusters.
     :return: a list of integers giving the predicted cluster
         for every <p> element.
     """
@@ -60,13 +67,39 @@ def page_paragraphs(p_objects):
                                    affinity="l1").fit_predict(X)
 
     # THIRD STEP (OPTIONAL): Draw the clusters over a new HTML document
-    n_clusters = pred.max() + 1
-    clusters = [[] for _ in range(n_clusters)]
-    for i, prediction in enumerate(pred):
-        clusters[prediction].append(p_objects[i])
+    if draw_clusters:
+        n_clusters = pred.max() + 1
+        clusters = [[] for _ in range(n_clusters)]
+        for i, prediction in enumerate(pred):
+            clusters[prediction].append(p_objects[i])
+        cmap = cm.get_cmap('tab20')
+        for k, cluster in enumerate(clusters):
+            # Assign a color to the cluster
+            color = cmap(k / n_clusters, bytes=True)
+            # Assign that color as background to all elements in that cluster
+            for elem in cluster:
+                elem['style'] += ";background-color:rgba({}, {}, {}, 0.3);".format(*color)
+
+    return pred
 
 
 if __name__ == "__main__":
+    print("Reading HTML file")
     with open(HTML_FILE, "r") as reme_html:
         full_html = reme_html.read()
-    soup = BeautifulSoup(full_html)
+    soup = BeautifulSoup(full_html, features="html.parser")
+    print("Done")
+    print("Parsing...")
+
+    # Divide into pages
+    pages = soup.body.find_all('div', recursive=False)
+    # Apply the clustering to every page
+    for k, page in enumerate(pages):
+        clusters = page_paragraphs(page.find_all('p', recursive=False),
+                                   draw_clusters=DRAW_CLUSTERS)
+        print(f'{k + 1} / {len(pages)} Pages processed                ', end="\r")
+
+    # Save the new HTML file over which the clusters have been drawed
+    with open(NEW_HTML_FILE, "w") as file:
+        file.write(str(soup))
+    print("Done")
