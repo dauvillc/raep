4b20161a47a62c8b3acc5b3dded6f06a3eff44ef
Clément Dauvilliers <dauvilliersclem@gmail.com>
2022-08-31 13:50:30 +0200

    Fixed the order of the 'locations list' and removed empty HTML tags

--- reme_clustering.py
+++ reme_clustering.py
@@ -20,16 +20,19 @@ from sklearn.mixture import GaussianMixture
 from sklearn.cluster import AgglomerativeClustering
 from scipy.spatial import distance_matrix
 
-# Path to the full HTML version of the REME pdf.
-HTML_FILE = os.path.join("docs", "rep_metiers-12.html")
-# Path to the new HTML file over which the clusters have been drawed
-NEW_HTML_FILE = os.path.join("visu_docs", "new_rep_metiers-12.html")
+# Path to the pages of the HTML version
+# This must be a directory in which each file is a specific
+# page xxxx-{number of page}.html
+# This is the output of pdftohtml if the -s option isn't used
+HTML_PAGES_REP = os.path.join("docs", "html_pages")
+# Directory of the new HTML pages over which the clusters have been drawed
+NEW_HTML_REP = "visu_docs"
 # Whether to draw the clusters on a new HTML file
 DRAW_CLUSTERS = True
 
 # Minimal distance in pixels between two clusters for them
 # to be split during the clustering algorithm.
-CLUSTERING_DISTANCE_THRESHOLD = 37.5
+CLUSTERING_DISTANCE_THRESHOLD = 27.5
 
 
 def get_pos(p_object):
@@ -53,10 +56,8 @@ def page_paragraphs(p_objects, draw_clusters=False):
         for every <p> element.
     """
     # FIRST STEP: extract the locations on the page of every <p>
+    locations = [get_pos(par) for par in p_objects]
     # Map (top, left) --> <p> object
-    par_dict = {get_pos(par): par for par in p_objects}
-    # List of all (top, left) locations
-    locations = list(par_dict.keys())
     tops, lefts = [x for x, y in locations], [y for x, y in locations]
 
     # SECOND STEP: Clustering
@@ -84,22 +85,25 @@ def page_paragraphs(p_objects, draw_clusters=False):
 
 
 if __name__ == "__main__":
-    print("Reading HTML file")
-    with open(HTML_FILE, "r") as reme_html:
-        full_html = reme_html.read()
-    soup = BeautifulSoup(full_html, features="html.parser")
-    print("Done")
-    print("Parsing...")
-
-    # Divide into pages
-    pages = soup.body.find_all('div', recursive=False)
     # Apply the clustering to every page
-    for k, page in enumerate(pages):
-        clusters = page_paragraphs(page.find_all('p', recursive=False),
-                                   draw_clusters=DRAW_CLUSTERS)
-        print(f'{k + 1} / {len(pages)} Pages processed                ', end="\r")
+    for k, page_path in enumerate(sorted(os.listdir(HTML_PAGES_REP))):
+        print(f"Reading HTML file {page_path}")
+        with open(os.path.join(HTML_PAGES_REP, page_path), "r") as page_html:
+            page = page_html.read()
+        soup = BeautifulSoup(page, features="html.parser")
+        print("Done")
+        print("Parsing...")
 
-    # Save the new HTML file over which the clusters have been drawed
-    with open(NEW_HTML_FILE, "w") as file:
-        file.write(str(soup))
+        # Removes all tags in the page empty of any human-readable text
+        for tag in soup.find_all():
+            if len(tag.get_text(strip=True)) == 0:
+                tag.extract()
+
+        # Retrieves all <p> elements in the page and performs the clustering
+        p_elements = soup.body.find('div').find_all('p', recursive=False)
+        clusters = page_paragraphs(p_elements, draw_clusters=DRAW_CLUSTERS)
+
+        # Save the new HTML file over which the clusters have been drawed
+        with open(os.path.join(NEW_HTML_REP, page_path), "w") as file:
+            file.write(str(soup))
     print("Done")
